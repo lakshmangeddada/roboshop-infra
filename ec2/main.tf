4beab93d1803a54e9fa9a51ae64b22a4f4ba723f
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-14 23:37:52 +0530

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,15 +2,15 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-ansible"
-  owners      = [data.aws_caller_identity.current.account_id]
+  name_regex = "devops-ansible"
+  owners = [data.aws_caller_identity.current.account_id]
 }
 
 
-resource "aws_instance" "ec2" {
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.sg.id]
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
   tags = {
     Name = var.component
   }
@@ -18,37 +18,36 @@ resource "aws_instance" "ec2" {
 
 resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
+
     connection {
-      host     = aws_instance.ec2.public_ip
+      host     = aws_instance.instance.public_ip
       user     = "centos"
       password = "DevOps321"
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/lakshmangeddada/ansible-roboshop roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/lakshmangeddada/robohsop-ansible-practice.git roboshop.yml -e role_name=${var.component}"
     ]
-
   }
-
 }
 
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
-  description = "Allow TLS inbound traffic"
+  description = "All"
 
   ingress {
-    description = "ALL"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "ALL"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -56,16 +55,20 @@ resource "aws_security_group" "sg" {
   }
 }
 
+
 resource "aws_route53_record" "record" {
   zone_id = "Z10202231Q9C3TKFTZOQE"
-  name    = "${var.component}-dev.devopsb71.online"
+  name    = "${var.component}-${var.env}.devops71.tech"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.ec2.private_ip]
+  records = [aws_instance.instance.private_ip]
 }
+
 
 variable "component" {}
 variable "instance_type" {}
 variable "env" {
   default = "dev"
 }
+
+
