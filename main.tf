a4ce915bfa5d5005300b8ca6e2010af80b5809e9
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-16 21:57:54 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "ec2" {
-  for_each = var.instances
-  source = "./ec2"
-  component = each.value["name"]
+  for_each      = var.instances
+  source        = "./ec2"
+  component     = each.value["name"]
   instance_type = each.value["type"]
+  env           = var.env
+  monitor       = try(each.value["monitor"], false)
 }
